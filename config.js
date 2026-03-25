338d0ee856a419e9fd9dadb03d8c330b19c34988
jokertalor-cpu <jokertalor@gmail.com>
2026-03-25 21:19:38 +0630

    Delete config.js

--- config.js
+++
@@ -1,7 +0,0 @@
-// config.js
-const CONFIG = {
-    SUPABASE_URL: 'https://cmguamftohgcgwdggwde.supabase.co',
-    SUPABASE_KEY: 'sb_publishable_xiWtoACTDkbfjmz9RMxWdw_R5W1UtZ9'
-};
-
-const supabase = window.supabase.createClient(CONFIG.SUPABASE_URL, CONFIG.SUPABASE_KEY);// config.js
